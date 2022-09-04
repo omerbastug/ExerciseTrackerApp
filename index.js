02a2359c9ebb9d169db86746c366d5a8dfb89b3d
omerbastug <bomersami@gmail.com>
2022-09-05 01:48:55 +0200

    logging user exercise by user id

--- index.js
+++ index.js
@@ -25,7 +25,37 @@ app.post("/api/users", (req,res) => {
   res.json({"username" : username, "_id" : id+""})
 })
 
+// get all users
+app.get("/api/users", (req,res) => {
 
+  let responseArray = new Array();
+  for(let i = 0; i<users.length; i++){
+    responseArray.push({"username": users.at(i), "_id": i+""})
+  }
+  res.send(responseArray)
+})
+
+let exercises = new Array();
+app.post("/api/users/:_id/exercises",(req,res) => {
+  let id = req.params._id;
+  let date = req.body.date ? new Date(req.body.date) : new Date();
+  if(id >= users.length) return res.json({"error": "User does not exist"})
+  
+  exercises.push({
+    "date" : date.toDateString(),
+    "duration" : parseInt(req.body.duration),
+    "description" : req.body.description,
+    "usersID" : id+""
+  })
+  
+  res.json({
+    "_id" : id+"",
+    "username" : users.at(id),
+    "date" : date.toDateString(),
+    "duration" : parseInt(req.body.duration),
+    "description" : req.body.description
+  })
+})
 
 
 
