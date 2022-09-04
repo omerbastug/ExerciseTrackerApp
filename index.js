cd74d7ea0a31c42c3d8cb9f45efcdb4de2604afe
omerbastug <bomersami@gmail.com>
2022-09-05 01:47:07 +0200

    api for getting all users

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
 
+let users = new Array();
+
+app.use(bodyParser.urlencoded({extended : true}));
+app.use(bodyParser.json());
+
+// create user
+app.post("/api/users", (req,res) => {
+  let username = req.body.username;
+  let id = users.findIndex((element) => element === username)
+  if(id === -1){
+    users.push(username);
+    id = users.length -1;
+  }
+  res.json({"username" : username, "_id" : id+""})
+})
+
 
 
 
