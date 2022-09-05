875e523d2c759d1044bf01efc0cfd7657cc326a2
omerbastug <bomersami@gmail.com>
2022-09-05 02:07:50 +0200

    changed port

--- index.js
+++ index.js
@@ -91,6 +91,6 @@ app.get("/api/users/:_id/logs", (req,res) => {
 })
 
 
-const listener = app.listen(process.env.PORT || 3000, () => {
+const listener = app.listen(process.env.YOUR_PORT || process.env.PORT || 80,process.env.YOUR_HOST || '0.0.0.0', () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
